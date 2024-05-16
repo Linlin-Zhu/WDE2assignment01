9f969d68c3b8d85c345201211750e99b1cbdd97e
Linlin Zhu <1695420@edu.vaniercollege.qc.ca>
2024-05-15 22:51:42 -0400

    apply SQL functions

--- assignment1.sql
+++ assignment1.sql
@@ -12,3 +12,9 @@ VALUES
     (1, 'Cardinal', 'Erichsen', 'Skagen 21', 'Stavanger'),
     (2, 'Greasy', 'Olsen', 'Gateveien 15', 'Sandnes'),
     (3, 'Tasty', 'Egan', 'Streetroad 19B', 'Liverpool');
+
+SELECT CustomerId, CONCAT(FirstName, ' ', LastName) AS CustomerName, Address, City FROM Customers;
+
+SELECT CustomerId, LastName, FirstName, Address, REPLACE(City, City, 'Montreal') AS City FROM Customers;
+
+SELECT CustomerId, TRIM(LastName) AS LastName, FirstName, Address, City FROM Customers;
